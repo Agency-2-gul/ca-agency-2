70b00d1e5eb720676db00e42bc1d2fc668582644
Kiretwo <eritor00991@stud.noroff.no>
2025-03-18 16:05:14 +0100

    Add custom color palette to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-bg": "#FFFFFF",
+        "orange-gradient": "linear-gradient(to right, #E64D20 0%, #F67B39 77%",
+        "light-sections": "#F7F7F7",
+        "primary-text": "#333333",
+        "footer-text": "#6C757D",
+        "accent-orange": "#F67B39",
+        "primary-text-faded": "rgba(51, 51, 51, 0.7)",
+        "progress-bar-bg": "rgba(219, 219, 219, 0.8)",
+        "progress-bar-undertext": "rgba(51, 51, 51, 0.45)",
+        "calorie-tracker-icon": "#B4B4B4",
+      },
+    },
   },
   plugins: [],
 };
