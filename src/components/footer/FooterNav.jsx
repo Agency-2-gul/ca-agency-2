f79e5db9ca6f0d4338c022977decac932ec80ff9
GreenGoblin01002 <erlhal01002@stud.noroff.no>
2025-03-20 14:34:46 +0100

    footer-icon fix

--- src/components/footer/FooterNav.jsx
+++ src/components/footer/FooterNav.jsx
@@ -4,7 +4,7 @@ import { BiSolidPieChartAlt2 } from 'react-icons/bi';
 import { FaBook } from 'react-icons/fa';
 import { FaCirclePlus } from 'react-icons/fa6';
 import { BsGraphUpArrow } from 'react-icons/bs';
-import { CgProfile } from "react-icons/cg";
+import { CgProfile } from 'react-icons/cg';
 
 export default function FooterNav() {
   return (
@@ -18,9 +18,20 @@ export default function FooterNav() {
         <FaBook className="icon" />{' '}
         <span className="icon-text">Oppskrifter</span>
       </Link>
-
+      <svg width="0" height="0" className="absolute">
+        <defs>
+          <linearGradient id="gradientId" x1="0%" y1="0%" x2="100%" y2="100%">
+            <stop stopColor="#E64D20" offset="0%" /> {/* Tailwind blue-500 */}
+            <stop stopColor="#F67B39" offset="100%" />{' '}
+            {/* Tailwind purple-500 */}
+          </linearGradient>
+        </defs>
+      </svg>
       <div className="icon-div">
-        <FaCirclePlus className="center-icon" />
+        <FaCirclePlus
+          className="center-icon"
+          style={{ fill: 'url(#gradientId)' }}
+        />
       </div>
       <div className="icon-div">
         <BsGraphUpArrow className="icon icon-stroke" />{' '}
