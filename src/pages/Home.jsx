22bdd1a860666f4615ad04cbd2f23965d61f0049
Ingebrigt Furnes Bøe <ingebrigt03@gmail.com>
2025-03-24 15:30:40 +0100

    made working weight graph

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import CalorieTracker from '../components/home/CalorieTracker';
+import WeightVisualization from '../components/weight-tracker/WeightVisualization';
 
 const Home = () => {
   return (
     <div>
       <CalorieTracker />
+      <WeightVisualization />
     </div>
   );
 };
