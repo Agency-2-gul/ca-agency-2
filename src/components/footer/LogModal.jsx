1e3e8b625e5b0f07cbc14aaaded5470163a6628c
GreenGoblin01002 <erlhal01002@stud.noroff.no>
2025-03-28 17:07:45 +0100

    logModal now utalizes mealselectormodal to redirect to logProduct/meal

--- src/components/footer/LogModal.jsx
+++ src/components/footer/LogModal.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react';
-import { FaCheckCircle } from 'react-icons/fa';
-import useProductSearch from '../../hooks/useProductSearch';
-import useLogProducts from '../../hooks/useLogProducts';
+import { use } from 'react';
+import MealSelectionModal from '../ean-logging/MealSelectionModal';
+import { useNavigate } from 'react-router-dom';
 
 const LogModal = ({ setIsModalOpen }) => {
+  const navigate = useNavigate();
   const onClose = () => setIsModalOpen(false);
-
+  const MEAL_OPTIONS = ['Frokost', 'Lunsj', 'Middag', 'Snacks', 'Kvelds'];
+  const handleConfirm = (meal) => {
+    navigate(`/log-products/${encodeURIComponent(meal)}`);
+    setIsModalOpen(false);
+  };
   return (
-    <div className="absolute bg-white w-[400px] max-h-[400px] overflow-y-auto top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-xl shadow-lg p-6 z-1000"></div>
+    <>
+      <MealSelectionModal
+        isOpen={true}
+        onClose={onClose}
+        onConfirm={handleConfirm}
+      />
+    </>
   );
 };
 
