a3b3feb7654c7c945f5590093f0ea035da5fffef
GreenGoblin01002 <erlhal01002@stud.noroff.no>
2025-04-04 14:31:27 +0200

    new style to navbar

--- src/components/recepies/recepies-main/NavBar.jsx
+++ src/components/recepies/recepies-main/NavBar.jsx
@@ -2,25 +2,25 @@ const NavBar = ({ setActiveTab, activeTab }) => {
   return (
     <div className="flex justify-around p-2 rounded-lg ">
       <button
-        className={`p-2 ${activeTab === 'all' ? 'text-orange-500 font-bold' : 'text-gray-500'}`}
+        className={`p-2 transition-all duration-150  ${activeTab === 'all' ? 'border-b-2 border-orange-500 font-bold' : 'text-gray-500'}`}
         onClick={() => setActiveTab('all')}
       >
         Alle
       </button>
       <button
-        className={`p-2 ${activeTab === 'mostLiked' ? 'text-orange-500 font-bold' : 'text-gray-500'}`}
+        className={`p-2 transition-all duration-150 ${activeTab === 'mostLiked' ? 'border-b-2 border-orange-500 font-bold' : 'text-gray-500'}`}
         onClick={() => setActiveTab('mostLiked')}
       >
         Mest Likt
       </button>
       <button
-        className={`p-2 ${activeTab === 'newest' ? 'text-orange-500 font-bold' : 'text-gray-500'}`}
+        className={`p-2 transition-all duration-150 ${activeTab === 'newest' ? 'border-b-2 border-orange-500 font-bold' : 'text-gray-500'}`}
         onClick={() => setActiveTab('newest')}
       >
         Nyeste
       </button>
       <button
-        className={`p-2 ${activeTab === 'myRecepies' ? 'text-orange-500 font-bold' : 'text-gray-500'}`}
+        className={`p-2 transition-all duration-150 ${activeTab === 'myRecepies' ? 'border-b-2 border-orange-500 font-bold' : 'text-gray-500'}`}
         onClick={() => setActiveTab('myRecepies')}
       >
         Mine Oppskrifter
